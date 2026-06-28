8ea064a35c37f7b420bf34aded006beb3c7ed67b
Yuki0123 <yuki.kobayashi@gmail.com>
2026-06-28 14:41:54 +0900

    改善: 静的ファイルのストレージ設定を追加し、マニフェストストレージを使用するように変更

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
